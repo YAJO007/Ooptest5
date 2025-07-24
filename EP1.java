a99fac42a9ef2bc40a9c0aefb09594212e70de17
Hexxxxxxx <67011212055@msu.ac.th>
2025-07-25 00:11:30 +0700

    Update EP1.java

--- EP1.java
+++ EP1.java
@@ -3,9 +3,10 @@ class EP1 {
 
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
+        int b = 2024235435;
         int c = a + b;
         System.out.println(c);
+
 
 
     }
